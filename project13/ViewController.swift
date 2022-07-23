c624fdb51798955e77b3fcde878dfc64e363a36b
Anders R <andersen@futurmap.com>
2022-07-23 15:45:01 +0300

    Init struct

---
+++ project13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  project13
+//
+//  Created by Madiapps on 23/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var intesity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
